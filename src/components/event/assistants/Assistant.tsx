8e761fac01b34b375b7712ed9c4028e266ad1e67
Annsof <103266603+Annsof@users.noreply.github.com>
2024-06-03 09:46:48 -0500

    adding little fix

--- src/components/event/assistants/Assistant.tsx
+++ src/components/event/assistants/Assistant.tsx
@@ -8,22 +8,25 @@ interface Props {
 
 export const AssistantItem = ( { assistant }: Props) => {
 
+	const [assistantLocal, setAssistantLocal] = useState<Assistant>();
+
 	useEffect(() => { 
 		const fetchingData = async () => {
 			console.log(`Fetching asssistant with ID: ${assistant}`);
 			const response = await axiosInstance.get(`/assistants/${assistant}`);
-			console.log('Event data:', response.data);
+			console.log(response.data);
+			setAssistantLocal(response.data);
 		}
 
 		fetchingData();
         
-	}, [])
+	}, []);
 
     return (
         <div className="flex items-center my-4 border-b-2 border-gray-100 pb-4">
 			<div className="ml-4">
 				<p className="font-semibold text-gray-700">
-					{assistant}
+					{assistantLocal?.full_name}
 				</p>
 			</div>
 		</div>
