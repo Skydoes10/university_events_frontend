176790fc90f34ecc3e1a85e8be312887e4b2c035
Annsof <103266603+Annsof@users.noreply.github.com>
2024-06-04 01:32:15 -0500

    adding assistant token

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,38 +1,29 @@
 'use client';
 
 import Link from 'next/link';
-// import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
-
-// import { useUIStore } from "@/store";
 import { titleFonts } from '@/config/fonts';
 
 export const Navbar = () => {
-	//   const toggleSideMenu = useUIStore((state) => state.toggleSideMenu);
+	const handleLogout = () => {
+		localStorage.setItem('token', "");
+	};
 
 	return (
 		<nav className="flex px-5 py-4 justify-between items-center w-full shadow-sm">
 			<div>
 				{/* Logo */}
 				<Link href="/">
-					<span
-						className={`${titleFonts.className} antialiased font-bold`}
-					>
+					<span className={`${titleFonts.className} antialiased font-bold`}>
 						Universidad
 					</span>
 				</Link>
 			</div>
 			{/* Center Menu */}
 			<div className="hidden sm:block">
-				<Link
-					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-					href="/"
-				>
+				<Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/">
 					Eventos
 				</Link>
-				<Link
-					className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
-					href="/nuevo-evento"
-				>
+				<Link className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" href="/nuevo-evento">
 					Nuevo Evento
 				</Link>
 			</div>
@@ -41,6 +32,7 @@ export const Navbar = () => {
 				<Link
 					className="m-2 p-2 rounded-md transition-all hover:bg-red-500 hover:text-white"
 					href="/iniciar-sesion"
+					onClick={handleLogout}
 				>
 					Cerrar Sesión
 				</Link>
