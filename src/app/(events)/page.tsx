782c5a6f80ebb2e6f82f291c25f4141b3e29a188
SebasHM1 <SebastianHMP18@gmail.com>
2024-06-04 02:11:05 -0500

    Iniciando comentarios

--- src/app/(events)/page.tsx
+++ src/app/(events)/page.tsx
@@ -5,6 +5,9 @@ import { EventList, Title } from '@/components';
 import axiosInstance from '@/utils/axiosInstance';
 import { Event } from '@/interfaces';
 import { EventListItem } from '@/components';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+
 
 interface EventListProps {
     events: Event[];
@@ -14,6 +17,8 @@ export default function EventsPage() {
     const [events, setEvents] = useState<Event[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+
+    const router = useRouter();
 
     useEffect(() => {
         const fetchEvents = async () => {
