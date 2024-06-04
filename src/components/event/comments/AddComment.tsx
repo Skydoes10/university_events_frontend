4b10584cf09ebf97a1ca415f49c922d6ed0dbf83
Skydoes10 <ricardoms1909@gmail.com>
2024-06-04 02:15:41 -0500

    chore: Refactor AddComment component to remove assistantId parameter and add authorization token

--- src/components/event/comments/AddComment.tsx
+++ src/components/event/comments/AddComment.tsx
@@ -2,52 +2,61 @@ import { useState } from 'react';
 import axiosInstance from '@/utils/axiosInstance';
 
 interface Props {
-  eventId: string;
-  assistantId: string;
-  onCommentAdded: (comment: Comment) => void;
+	eventId: string;
+	onCommentAdded: (comment: Comment) => void;
 }
 
-export const AddComment = ({ eventId, assistantId, onCommentAdded }: Props) => {
-  const [text, setText] = useState('');
+export const AddComment = ({ eventId, onCommentAdded }: Props) => {
+	const [text, setText] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e: any) => {
+		e.preventDefault();
 
-    if (text.length < 4 || text.length > 100) {
-      alert('El comentario debe tener entre 4 y 100 caracteres.');
-      return;
-    }
+		if (text.length < 4 || text.length > 100) {
+			alert('El comentario debe tener entre 4 y 100 caracteres.');
+			return;
+		}
 
-    try {
-      const response = await axiosInstance.post(`/events/comment/${eventId}/${assistantId}`, {
-        text
-      });
-      onCommentAdded(response.data);
-      setText('');
-    } catch (error) {
-      console.error('Error adding comment:', error);
-      alert('Hubo un error al agregar el comentario.');
-    }
-  };
+		const token = localStorage.getItem('token');
 
-  return (
-    <div className="flex items-center my-4 border-b-2 border-gray-100 pb-4">
-      <form className="flex flex-col w-full" onSubmit={handleSubmit}>
-        <textarea
-          className="p-2 border border-gray-200 rounded-md focus:outline-none focus:shadow-outline"
-          placeholder="Escribe un comentario..."
-          maxLength={100}
-          minLength={4}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        ></textarea>
-        <button
-          type="submit"
-          className="w-1/5 mt-4 btn-primary text-white px-4 py-2 rounded-md focus:outline-none focus:shadow-outline"
-        >
-          Enviar
-        </button>
-      </form>
-    </div>
-  );
+		try {
+			const response = await axiosInstance.post(
+				`/events/comment/${eventId}`,
+				{
+					comment: text,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+			onCommentAdded(response.data);
+			setText('');
+		} catch (error) {
+			console.error('Error adding comment:', error);
+			alert('Hubo un error al agregar el comentario.');
+		}
+	};
+
+	return (
+		<div className="flex items-center my-4 border-b-2 border-gray-100 pb-4">
+			<form className="flex flex-col w-full" onSubmit={handleSubmit}>
+				<textarea
+					className="p-2 border border-gray-200 rounded-md focus:outline-none focus:shadow-outline"
+					placeholder="Escribe un comentario..."
+					maxLength={100}
+					minLength={4}
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				></textarea>
+				<button
+					type="submit"
+					className="w-1/5 mt-4 btn-primary text-white px-4 py-2 rounded-md focus:outline-none focus:shadow-outline"
+				>
+					Enviar
+				</button>
+			</form>
+		</div>
+	);
 };
